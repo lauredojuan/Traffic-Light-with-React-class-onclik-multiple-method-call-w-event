081e4569f5b22076d0c381b37df699f8d6e34177
Juan L <lauredojuan@gmail.com>
2020-07-29 20:57:28 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,14 +5,19 @@ import "bootstrap";
 export function Home() {
 	let isOn = true;
 
-	// onFunction();
+	function onFunction(event) {}
 
 	return (
 		<div className="container-fluid">
-			<div className="box d-flex flex-column mt-3">
-				<div className={`red m-2 ${isOn ? "" : "lightOn"}`} />
-				<div className="yellow m-2 lightOn" />
-				<div className="green m-2" />
+			<div className="box d-flex justify-content-center mt-3">
+				<div className="d-flex flex-column">
+					<div
+						onClick={onFunction}
+						className={`red m-2 ${isOn ? "lightOn" : ""}`}
+					/>
+					<div className="yellow m-2 lightOn" />
+					<div className="green m-2" />
+				</div>
 			</div>
 		</div>
 	);
