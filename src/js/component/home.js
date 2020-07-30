ec1399b32a28f14e935e43b5da4f6ddadb03c2eb
Juan L <lauredojuan@gmail.com>
2020-07-30 14:07:25 +0000

    working version

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,40 @@
-import React from "react";
-
+import React, { Component } from "react";
 
 //create your first component
-export function Home() {
-	let isOn = true;
 
-	function onFunction(event) {}
+export class Home extends Component {
+	constructor() {
+		super();
+		this.state = {
+			color: ""
+		};
+	}
+	render() {
+		let redOn = this.state.color === "red" ? "lightOn" : "";
+		let yellowOn = this.state.color === "yellow" ? "lightOn" : "";
+		let greenOn = this.state.color === "green" ? "lightOn" : "";
 
-	return (
-		<div className="container-fluid">
-			<div className="box d-flex justify-content-center mt-3">
-				<div className="d-flex flex-column">
-					<div
-						onClick={onFunction}
-						className={`red m-2 ${isOn ? "lightOn" : ""}`}
-					/>
-					<div className="yellow m-2 lightOn" />
-					<div className="green m-2" />
+		return (
+			<div className="container-fluid">
+				<div className="box d-flex justify-content-center mt-3">
+					<div className="d-flex flex-column">
+						<div
+							onClick={() => this.setState({ color: "red" })}
+							className={"red m-2 " + redOn}
+						/>
+
+						<div
+							onClick={() => this.setState({ color: "yellow" })}
+							className={"yellow m-2 " + yellowOn}
+						/>
+
+						<div
+							onClick={() => this.setState({ color: "green" })}
+							className={"green m-2 " + greenOn}
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
